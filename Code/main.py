03babd24c3038961877c69379f3a067f9d9d1882
Pablo Angulo <pab.angulo@outlook.com>
2020-08-27 01:11:31 -0600

    Added IMG adjust in 2D initial plotting

--- Code/main.py
+++ Code/main.py
@@ -1,17 +1,50 @@
 import myFuncs as Fn
+from pathlib import Path
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from tkinter import *
+from tkinter import filedialog
 
-# Get the file location from user starting from Data directory
-dataFilePath = input("Path to file from /Data:")
-preMarkerDict = Fn.PrepareData(dataFilePath)
+userWindow = Tk()
+userWindow.title('FaceCap Auto-Label')
+# Get the Data (CSV) file location from user
+dataFileName = StringVar()
+Label(userWindow, text="Select a Data file:").grid(column = 0, row = 0, pady=20)
+dataFileBtn = Button(userWindow, text='Browse Data Files', width=18)
+dataFileBtn.config(command=lambda: 
+   Fn.file_selection(dataFileName,"Select A Data File", [("CSV File", "*.csv")] ) 
+   )
+dataFileBtn.grid(column = 1, row = 0, pady=20)
+
+# Get the Layout file location from the user
+layoutFilePath = StringVar()
+Label(userWindow, text="Select a Layout file:").grid(column = 0, row = 2)
+layoutFileBtn = Button(userWindow, text='Browse Layout Files', width=18)
+layoutFileBtn.config(command=lambda:
+   Fn.file_selection(layoutFilePath, "Select A Layout File", [("PNG File", "*.png"),("JPEG File", "*.jpeg"),("JPG File", "*.jpg")] ) 
+   )
+layoutFileBtn.grid(column = 1, row = 2)
+
+usrMarkerCount = IntVar()
+Label(userWindow, text='Enter how many markers you are using:', font=('bold', 10)).grid(column= 0, row= 3)
+Entry(userWindow, textvariable=usrMarkerCount).grid(column= 1, row=3)
+
+initialPlotBtn = Button(userWindow, text='Initialize Labels')
+initialPlotBtn.config(command =lambda:
+   Fn.plotInitialLayout(dataFilePath= dataFileName.get(), layoutPath= layoutFilePath.get())
+   )
+initialPlotBtn.grid(column = 1, row = 4)
+
+userWindow.mainloop()
+preMarkerDict = Fn.PrepareData(Path(dataFileName.get()))
 # print('\n Marker Arrays')    
 # for key in preMarkerDict:
 #    print(key)
 #    print(preMarkerDict[key])
 
 preMarkerCount = len(preMarkerDict.keys())
+print("User Marker Count",usrMarkerCount.get())
 
 #Eucledian Distance between marker in different frames
 point1 = preMarkerDict['marker1'][0,:]
@@ -19,7 +52,6 @@ point2 = preMarkerDict['marker1'][1,:]
 dist = np.linalg.norm(point1 - point2)
 
 # ------------------------------~CODE TO PLOT DATA~-------------------------------------
-ax = plt.axes(projection='3d')
 
 # Data in X Dim
 xdata = np.array(preMarkerDict['marker1'][:,0])
@@ -34,35 +66,10 @@ zdata = np.array(preMarkerDict['marker1'][:,2])
 for i in range(2, preMarkerCount+1):
    zdata = np.concatenate((zdata,preMarkerDict['marker{}'.format(i)][:,2]),axis=0)
 
-# First frame Data in X Dim
-xfdata = np.array((preMarkerDict['marker1'][0,0], preMarkerDict['marker2'][0,0]))
-for i in range(3, preMarkerCount+1):
-   xfdata = np.concatenate(( xfdata,[preMarkerDict['marker{}'.format(i)][0,0]]),axis=0)
-#First frame Data in Y Dim
-yfdata = np.array((preMarkerDict['marker1'][0,1], preMarkerDict['marker2'][0,1]))
-for i in range(3, preMarkerCount+1):
-   yfdata = np.concatenate(( yfdata,[preMarkerDict['marker{}'.format(i)][0,1]]),axis=0)
-#First Frame Data in Z Dim
-zfdata = np.array((preMarkerDict['marker1'][0,2], preMarkerDict['marker2'][0,2]))
-for i in range(3, preMarkerCount+1):
-   zfdata = np.concatenate(( zfdata,[preMarkerDict['marker{}'.format(i)][0,2]]),axis=0)
-
-ax.scatter3D(xfdata, yfdata, zfdata, c=zfdata, cmap='plasma')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-# Z+ pointing into screen | X- pointing to right screen border
-plt.show()
-
-# 2D plot of the first frame data
-minX = np.amin(xfdata)
-minY = np.amin(yfdata)
-maxX = np.amax(xfdata)
-maxY = np.amax(yfdata)
-faceimg = plt.imread("./Code/Layout14.png")
-
-plt.scatter(xfdata, yfdata, cmap='plasma')
-plt.imshow(faceimg, zorder=0, extent=[-0.30, -0.09, 0.18, 0.40])
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.show()
+# ax = plt.axes(projection='3d')
+# ax.scatter3D(xfdata, yfdata, zfdata, c=zfdata, cmap='plasma')
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+# # Z+ pointing into screen | X- pointing to right screen border
+# plt.show()
--- Code/myFuncs.py
+++ Code/myFuncs.py
@@ -2,13 +2,17 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+from tkinter import *
+from tkinter import filedialog
 
 #FUNCTIONS AND CLASSES
 
+# Recieves CSV file path for Mocap Data and returns a Dict of the markers as keys and position matrixes as values (FramesxXYZ)
 def PrepareData(sessionFilePath):
 	# Use pandas to read CSV and save to DataFrame
 	# Drop unnecesary rows like Format, HexDecLabel and PositionLabel rows
-	path_toData = Path('readCSV.py').parent.parent/ 'Data' / sessionFilePath
+	path_toData = Path(sessionFilePath)
 	markersDF = pd.read_csv(path_toData.resolve(), skiprows=[0,1,2,4,5,6])
 
 	# Rename Frame and Time header labels as they are read as Unnamed
@@ -25,7 +29,7 @@ def PrepareData(sessionFilePath):
 	# Each marker has x(t), y(t) and z(t) vectors
 	FullMarkerCount = (FullColumnCount-1)/3
 	FullMarkerCount = int(FullMarkerCount)
-	print('\n Marker Count:', FullMarkerCount, '\n')
+	print('\nInitial Marker Count:', FullMarkerCount, '\n')
 
 	# Create Frame numpy Array
 	#FrameArray = markersDF.iloc[:, 0].to_numpy(copy=True)
@@ -48,6 +52,86 @@ def PrepareData(sessionFilePath):
 		# add the active marker matrix to the corresponding marker dictionary key
 		markerDict['marker{}'.format(currentMarkerIndex)] = activeMarker
 	return markerDict
+
+# Function to plot in 2D the first frame of FaceCap Data with a reference layout image
+# markerDict in format ['markerN':((X1,Y1,Z1),(X2,Y2,Z2),...), 'markerN+1':((X1,Y1,Z1),(X2,Y2,Z2),...)]
+def plotInitialLayout(dataFilePath, layoutPath):
+
+	preMarkerDict = PrepareData(Path(dataFilePath))
+	# print('\n Marker Arrays')    
+	# for key in preMarkerDict:
+	#    print(key)
+	#    print(preMarkerDict[key])
+
+	preMarkerCount = len(preMarkerDict.keys())
+
+	# First frame Data in X Dim
+	xfdata = np.array((preMarkerDict['marker1'][0,0], preMarkerDict['marker2'][0,0]))
+	for i in range(3, preMarkerCount+1):
+   		xfdata = np.concatenate(( xfdata,[preMarkerDict['marker{}'.format(i)][0,0]]),axis=0)
+	#First frame Data in Y Dim
+	yfdata = np.array((preMarkerDict['marker1'][0,1], preMarkerDict['marker2'][0,1]))
+	for i in range(3, preMarkerCount+1):
+		yfdata = np.concatenate(( yfdata,[preMarkerDict['marker{}'.format(i)][0,1]]),axis=0)
+	#First Frame Data in Z Dim
+	zfdata = np.array((preMarkerDict['marker1'][0,2], preMarkerDict['marker2'][0,2]))
+	for i in range(3, preMarkerCount+1):
+	   zfdata = np.concatenate(( zfdata,[preMarkerDict['marker{}'.format(i)][0,2]]),axis=0)
+
+	# 2D plot of the first frame data
+	minX = np.amin(xfdata)
+	minY = np.amin(yfdata)
+	maxX = np.amax(xfdata)
+	maxY = np.amax(yfdata)
+	print(minX, maxX, minY, maxY)
+	# Layout image for reference
+	faceimg = plt.imread(Path(layoutPath))
+
+	#creating subplots to use slider widgets on window
+	fig, ax = plt.subplots()
+	plt.subplots_adjust(left=0.1, bottom=0.35)
+	plotFigure = plt.scatter(xfdata, yfdata, cmap='plasma')
+	# extent in data units in order imshow(img, zorder=0, extent=[left, right, bottom, top])
+	# Default testing values [-0.30, -0.09, 0.18, 0.40]
+	layoutFig = plt.imshow(faceimg, zorder=0, extent=[-0.30, -0.09, 0.18, 0.40])
+	plt.xlabel('X')
+	plt.ylabel('Y')
+
+	minX_Slider = plt.axes([0.25, 0.1, 0.65, 0.03])
+	sl_minX = Slider(minX_Slider, 'IMG Left Lim', valmin=1.5*minX, valmax=0.5*minX, valinit=-0.30)
+
+	maxX_Slider = plt.axes([0.25, 0.15, 0.65, 0.03])
+	sl_maxX = Slider(maxX_Slider, 'IMG Right Lim', valmin=1.5*maxX, valmax=0.5*maxX, valinit=-0.09)
+
+	minY_Slider = plt.axes([0.25, 0.2, 0.65, 0.03])
+	sl_minY = Slider(minY_Slider, 'IMG Bottom Lim', valmin=0.5*minY, valmax=1.5*minY, valinit=0.18)
+
+	maxY_Slider = plt.axes([0.25, 0.25, 0.65, 0.03])
+	sl_maxY = Slider(maxY_Slider, 'IMG Top Lim', valmin=0.5*maxY, valmax=1.5*maxY, valinit=0.40)
+
+	def updateLims(val):
+		leftLim = sl_minX.val
+		rightLim = sl_maxX.val
+		bottomLim = sl_minY.val
+		topLim = sl_maxY.val
+
+		layoutFig.set_extent([leftLim, rightLim, bottomLim, topLim])
+	
+	sl_minX.on_changed(updateLims)
+	sl_maxX.on_changed(updateLims)
+	sl_minY.on_changed(updateLims)
+	sl_maxY.on_changed(updateLims)
+
+	plt.show()
+
+# function to select file on button press, wintitle is a Str and winfiletype is a tuple on format ("Title", "*.extension"),("Title2", "*.extension2")
+def file_selection(filenameVar, wintitle, winfiletype):
+	filename =  filedialog.askopenfilename(initialdir=Path(), title=wintitle, filetypes=winfiletype)
+	try:
+		print("Selected:", filename)
+		filenameVar.set(filename)
+	except:
+		print("No file selected")
 
 def LabelFirstFrame():
 	pass
